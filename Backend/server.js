23ea679b61696472246bec17d1cc508d87b6bef2
Shreyas Nair <shreyas.nair@zeuslearning.com>
2026-06-03 19:06:47 +0530

    Initialization of backend and database

---
+++ Backend/server.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+
+app.use(cors)
+app.use(express.json)
+
+mongoose.connect(process.env.MONGO_DB_URI)
+    .then(() => console.log('Connected to mongodb'))
+    .catch((err) => console.log("Error in connceting to db:", err));
+
+const itemSchema = new mongoose.Schema({
+    taskName: { type: String, required: true },
+    taskD: { type: String, default: '' },
+    dueD: { type: Date, required: true },
+    status: { type: Boolean, required: false }
+})
+
+const Item = mongoose.model('Item', itemSchema)
+
+app.get = ('/', (req, res) => {
+    res.send('Hello from express!');
+})
+
+app.get = ('/api/list', (req, res) => {
+    try {
+        const taskList = await Item.find()
+        res.json(taskList)
+    } catch (err) {
+        res.status(500).json({ message: 'Error:Cant Fetch Data', err });
+    }
+});
+
+app.get = ('/api/deleteTask', (req, res) => {
+    res.send('Hello from express!');
+})
+
+app.post = ('/api/createTask', (req, res) => {
+    try {
+        const newTask = new Item({
+            taskName: req.body.taskName,
+            taskD: req.body.taskD,
+            dueD: req.body.dueD,
+            status: req.body.status
+        })
+    } catch (err) {
+        res.status(500).json({message:'Error: Cannot post Data',err})
+    }
+})
+
+app.listen(Port, () => {
+    console.log("Express server is live")
+})
